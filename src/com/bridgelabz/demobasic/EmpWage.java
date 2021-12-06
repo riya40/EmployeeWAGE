d56dc3872c2d70b5e9bf5a2c384b0741a4f55a7b
riya40 <priyanunna20@gmail.com>
2021-12-06 20:46:55 +0530

    Added:Checking Employee is Present or Not

--- src/com/bridgelabz/demobasic/EmpWage.java
+++ src/com/bridgelabz/demobasic/EmpWage.java
@@ -5,5 +5,14 @@ public class EmpWage {
     public static void main(String[] args) {
 	// displaying the welcome Message
         System.out.println("Welcome To Employee Wage Computation");
+
+        //checking the Employee Presence
+        double rand = Math.floor(Math.random()*10)%2;
+        if(rand==1){
+            System.out.println("employee present");
+        }
+        else{
+            System.out.println("employee absent");
+        }
     }
 }
